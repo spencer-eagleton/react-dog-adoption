abc6b929cc954416ab7491adffdfe06314ae16a2
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2022-01-04 18:06:11 -0800

    created doglist function and call

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,3 +1,14 @@
+import { fetchDogs } from '../services/doglist';
+import { useState, useEffect } from 'react';
 export default function Home() {
+  const [dogs, setDogs] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchDogs();
+      setDogs(data);
+    };
+    fetchData();
+  }, []);
+  console.log(dogs);
   return <h1>Dog List</h1>;
 }
