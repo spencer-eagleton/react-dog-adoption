c6d9e6f5763a35228c36b1bcbd67769847614127
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2022-01-05 18:05:27 -0800

    wrote use effect for fetchdogby id not working yet need to fix params

--- src/Views/DogDetail.js
+++ src/Views/DogDetail.js
@@ -1,3 +1,21 @@
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
+import { checkError } from '../services/client';
+import { fetchDogById } from '../services/doglist';
+import Dog from '../Components/Dog';
 export default function DogDetail() {
-  return <h1>Dog Detail</h1>;
+  const params = useParams();
+  useEffect(() => {
+    const fetchData = async () => {
+      const resp = await fetchDogById(params.id);
+      return checkError(resp);
+    };
+    fetchData();
+  }, [params.id]);
+  return (
+    <>
+      <h1>Dog Detail</h1>
+      <Dog />
+    </>
+  );
 }
