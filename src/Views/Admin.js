cd2d209d41d67d5194ee6653db4af75cac018c7f
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2022-01-09 14:39:37 -0800

    started admin state

--- src/Views/Admin.js
+++ src/Views/Admin.js
@@ -1,5 +1,11 @@
 import AdminForm from '../Components/AdminForm';
+import { useState } from 'react';
 
 export default function Admin() {
+  const [name, setName] = useState('');
+  const [breed, setBreed] = useState('');
+  const [age, setAge] = useState('');
+  const [image, setImage] = useState('');
+  const [bio, setBio] = useState('');
   return <AdminForm />;
 }
