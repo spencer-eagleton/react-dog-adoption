88ef5cdbebb6487cee90108686eac4306a2c95d2
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2022-01-08 19:19:59 -0800

    refactored admin form

---
+++ src/Components/AdminForm.js
@@ -0,0 +1,30 @@
+export default function AdminForm() {
+  return (
+    <>
+      <div>Add a new pupperoo</div>
+      <form>
+        <label>
+          Name:
+          <input type="text" />
+        </label>
+        <label>
+          Breed:
+          <input type="text" />
+        </label>
+        <label>
+          Age:
+          <input type="text" />
+        </label>
+        <label>
+          Image:
+          <input type="text" />
+        </label>
+        <label>
+          Bio:
+          <input type="text" />
+        </label>
+        <button>Add Pupper</button>
+      </form>
+    </>
+  );
+}
--- src/Views/Admin.js
+++ src/Views/Admin.js
@@ -1,29 +1,5 @@
+import AdminForm from '../Components/AdminForm';
+
 export default function Admin() {
-  return (
-    <>
-      <div>Admin</div>
-      <form>
-        <label>
-          Name:
-          <input type="text" />
-        </label>
-        <label>
-          Breed:
-          <input type="text" />
-        </label>
-        <label>
-          Age:
-          <input type="text" />
-        </label>
-        <label>
-          Image:
-          <input type="text" />
-        </label>
-        <label>
-          Bio:
-          <input type="text" />
-        </label>
-      </form>
-    </>
-  );
+  return <AdminForm />;
 }
