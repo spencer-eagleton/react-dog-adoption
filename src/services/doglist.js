d2e7572c28478a77871831cdc1468f6214ea3af9
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2022-01-09 21:34:54 -0800

    added create dog call

--- src/services/doglist.js
+++ src/services/doglist.js
@@ -14,3 +14,11 @@ export async function updateDog(id, name, breed, age, image, bio) {
   const resp = await client.from('dogs').update({ name, breed, age, image, bio }).eq('id', id);
   return checkError(resp);
 }
+
+export async function createDog(name, breed, age, image, bio) {
+  const resp = await client
+    .from('dogs')
+    .insert({ name: name, breed: breed, age: age, image: image })
+    .single();
+  return resp;
+}
