4aaead14aef1c397f84793b8d96fc7f7c21bfe2b
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2022-01-09 19:53:08 -0800

    added updatedog call function

--- src/services/doglist.js
+++ src/services/doglist.js
@@ -9,3 +9,8 @@ export async function fetchDogById(id) {
   const resp = await client.from('dogs').select('*').eq('id', id).single();
   return checkError(resp);
 }
+
+export async function updateDog(id, name, breed, age, image, bio) {
+  const resp = await client.from('dogs').update({ name, breed, age, image, bio }).eq('id', id);
+  return checkError(resp);
+}
