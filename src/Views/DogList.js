07a43f0462577302a1c3272a2fcf82d09587bb93
spencer-eagleton <86867967+spencer-eagleton@users.noreply.github.com>
2022-01-13 14:39:12 -0800

    removed console log

--- src/Views/DogList.js
+++ src/Views/DogList.js
@@ -1,7 +1,6 @@
 import Dog from '../Components/Dog';
 import { Link } from 'react-router-dom';
 export default function DogList({ dogs }) {
-  console.log(dogs);
   return (
     <div>
       {dogs.map((dog) => (
